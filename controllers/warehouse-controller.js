c449d25c32a9104e8815644df7d4aa9b3adbdec9
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-13 14:47:24 -0700

    adds basic warehouses get feature

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,3 +1,14 @@
 import initKnex from "knex";
 import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
+
+const listAll = async (req, res) => {
+  try {
+    const allData = await knex("warehouses");
+    res.status(200).json(allData);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+  }
+};
+
+export { listAll };
