1cdbc8bfe71c39ab28880926c892c944dd23e4db
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-13 13:39:00 -0700

    adds imports to warehouse controller and route, imports cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
-
 app.listen(PORT, () => {
-    console.log(`running at http://localhost:${PORT}`);
+  console.log(`running at http://localhost:${PORT}`);
 });
---
+++ routes/warehouse-route.js
@@ -0,0 +1,3 @@
+import * as warehouseController from "../controllers/warehouse-controller.js";
+import express from "express";
+const router = express.Router();
