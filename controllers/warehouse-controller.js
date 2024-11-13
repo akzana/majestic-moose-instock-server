5d8caa1225ea05b148c1d1aff7d2b930adca17cf
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-13 15:06:09 -0700

    updates listAll to map only necessary data, not including time created or updated.

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -2,12 +2,24 @@ import initKnex from "knex";
 import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
-const listAll = async (req, res) => {
+const listAll = async (_req, res) => {
   try {
     const allData = await knex("warehouses");
-    res.status(200).json(allData);
+    const warehousesData = allData.map((warehouse) => ({
+      id: warehouse.id,
+      warehouse_name: warehouse.warehouse_name,
+      address: warehouse.address,
+      city: warehouse.city,
+      country: warehouse.country,
+      contact_name: warehouse.contact_name,
+      contact_position: warehouse.contact_position,
+      contact_phone: warehouse.contact_phone,
+      contact_email: warehouse.contact_email,
+    }));
+    res.status(200).json(warehousesData);
   } catch (err) {
-    res.status(400).send(`Error retrieving Warehouses: ${err}`);
+    res.status(500).send(`Server error retrieving Warehouses`);
+    console.error("Error getting list of all warehouses:", err);
   }
 };
 
