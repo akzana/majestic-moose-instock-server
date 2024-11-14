40405cdb00404221e0d024cdc24a0230bdab9237
Juliet Chuon | itsjuliet <j-chuon@hotmail.com>
2024-11-14 08:40:06 -0800

    remove cors in index.js as its added in dev

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import express from "express";
-import cors from 'cors';
 import warehouseRoutes from "./routes/warehouse-route.js";
 
 const app = express();
@@ -8,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
-app.use(cors());
+
 app.use("/api/warehouses", warehouseRoutes);
 
 
