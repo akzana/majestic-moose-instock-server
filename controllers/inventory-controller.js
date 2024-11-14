1d643a42b70414e74742e540fda8a03ba7bfa8dd
akzana <akzanaklepsch@outlook.com>
2024-11-14 10:03:39 -0800

    🔨 add inventory route & controller

---
+++ controllers/inventory-controller.js
@@ -0,0 +1,27 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+const listAll = async (_req, res) => {
+    try {
+        const allData = await knex("inventories");
+        const inventoryData = allData.map((inventory) => ({
+            id: inventory.id,
+            warehouse_name: inventory.warehouse_name,
+            item_name: inventory.item_name,
+            category: inventory.category,
+            status: inventory.status,
+            quantity: inventory.quantity
+        }));
+        res
+            .status(200)
+            .json(inventoryData);
+    } catch (err) {
+        res
+            .status(500)
+            .send(`Server error retrieving inventorys`);
+        console.error("Error getting list of all inventorys:", err);
+    }
+};
+
+export { listAll };
