8514c00a51e4d028abb962d485617737b3fd6dfc
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-14 23:34:09 -0700

    adds status codes and validation for item.id

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -23,25 +23,35 @@ const listAll = async (_req, res) => {
 };
 
 const listOne = async (req, res) => {
-  const allData = await knex("inventories")
-    .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
-    .select("inventories.*", "warehouses.warehouse_name");
+  try {
+    const allData = await knex("inventories")
+      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+      .select("inventories.*", "warehouses.warehouse_name");
 
-  const inventoryData = allData.map((inventory) => ({
-    id: inventory.id,
-    warehouse_name: inventory.warehouse_name,
-    item_name: inventory.item_name,
-    desription: inventory.description,
-    category: inventory.category,
-    status: inventory.status,
-    quantity: inventory.quantity,
-  }));
-  console.log(typeof req.params.id);
-  const foundItem = inventoryData.find(
-    (item) => item.id === Number(req.params.id)
-  );
-  console.log(foundItem);
-  res.status(200).json(foundItem);
+    const inventoryData = allData.map((inventory) => ({
+      id: inventory.id,
+      warehouse_name: inventory.warehouse_name,
+      item_name: inventory.item_name,
+      description: inventory.description,
+      category: inventory.category,
+      status: inventory.status,
+      quantity: inventory.quantity,
+    }));
+
+    const foundItem = inventoryData.find(
+      (item) => item.id === Number(req.params.id)
+    );
+    if (foundItem) {
+      res.status(200).json(foundItem);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Item with ID ${req.params.id} not found` });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ Message: "Server error getting inventory item" });
+  }
 };
 
 export { listAll, listOne };
