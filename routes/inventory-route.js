499a32601384f9123f0b72b33f4db4b053149e49
akzana <akzanaklepsch@outlook.com>
2024-11-14 16:55:18 -0800

    ✏️ update code structure

--- routes/inventory-route.js
+++ routes/inventory-route.js
@@ -2,6 +2,8 @@ import * as inventoryController from "../controllers/inventory-controller.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/inventories").get(inventoryController.listAll);
+router
+    .route("/inventories")
+    .get(inventoryController.listAll);
 
 export default router;
