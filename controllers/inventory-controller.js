127d5594fe4e0859d1fb9f3d6a20674b9f315951
akzana <akzanaklepsch@outlook.com>
2024-11-15 16:06:46 -0800

    add warehouse.id in res.body for logic on front end; related to front-end MMCTC-24

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -3,55 +3,56 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 const listAll = async (_req, res) => {
-  try {
-    const allData = await knex("inventories")
-      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
-      .select("inventories.*", "warehouses.warehouse_name");
-    const inventoryData = allData.map((inventory) => ({
-      id: inventory.id,
-      warehouse_name: inventory.warehouse_name,
-      item_name: inventory.item_name,
-      category: inventory.category,
-      status: inventory.status,
-      quantity: inventory.quantity,
-    }));
-    res.status(200).json(inventoryData);
-  } catch (err) {
-    res.status(500).send(`Server error retrieving inventories`);
-    console.error("Error getting list of all inventorys:", err);
-  }
+    try {
+        const allData = await knex("inventories")
+            .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+            .select("inventories.*", "warehouses.warehouse_name", "warehouses.id");
+        const inventoryData = allData.map((inventory) => ({
+            id: inventory.id,
+            warehouse_name: inventory.warehouse_name,
+            warehouseId: inventory.warehouse_id,
+            item_name: inventory.item_name,
+            category: inventory.category,
+            status: inventory.status,
+            quantity: inventory.quantity,
+        }));
+        res.status(200).json(inventoryData);
+    } catch (err) {
+        res.status(500).send(`Server error retrieving inventories`);
+        console.error("Error getting list of all inventorys:", err);
+    }
 };
 
 const listOne = async (req, res) => {
-  try {
-    const allData = await knex("inventories")
-      .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
-      .select("inventories.*", "warehouses.warehouse_name");
+    try {
+        const allData = await knex("inventories")
+            .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+            .select("inventories.*", "warehouses.warehouse_name");
 
-    const inventoryData = allData.map((inventory) => ({
-      id: inventory.id,
-      warehouse_name: inventory.warehouse_name,
-      item_name: inventory.item_name,
-      description: inventory.description,
-      category: inventory.category,
-      status: inventory.status,
-      quantity: inventory.quantity,
-    }));
+        const inventoryData = allData.map((inventory) => ({
+            id: inventory.id,
+            warehouse_name: inventory.warehouse_name,
+            item_name: inventory.item_name,
+            description: inventory.description,
+            category: inventory.category,
+            status: inventory.status,
+            quantity: inventory.quantity,
+        }));
 
-    const foundItem = inventoryData.find(
-      (item) => item.id === Number(req.params.id)
-    );
-    if (foundItem) {
-      res.status(200).json(foundItem);
-    } else {
-      res
-        .status(404)
-        .json({ message: `Item with ID ${req.params.id} not found` });
+        const foundItem = inventoryData.find(
+            (item) => item.id === Number(req.params.id)
+        );
+        if (foundItem) {
+            res.status(200).json(foundItem);
+        } else {
+            res
+                .status(404)
+                .json({ message: `Item with ID ${req.params.id} not found` });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ Message: "Server error getting inventory item" });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ Message: "Server error getting inventory item" });
-  }
 };
 
 export { listAll, listOne };
